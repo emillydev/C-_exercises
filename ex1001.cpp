7b7f2cd6e376bf8c79090a793281e0ee35d9842e
Emilly Victória <82069194+emillydev@users.noreply.github.com>
2022-02-16 20:57:26 -0300

    up

--- ex1001.cpp
+++ ex1001.cpp
@@ -1,5 +1,5 @@
 /*Read 2 variables, named A and B and make the sum of these two variables, assigning its result to the variable X. 
-Print X as shown below. Print endline after the result otherwise you will get “Presentation Error”.*/
+Print X as shown below. Print endline after the result otherwise you will get “Presentation Error”.*/ 
 
 #include<stdio.h>
 
