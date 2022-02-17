85a21f7dc8000f02359dc2e5312730cccdbb484f
Emilly Victória <82069194+emillydev@users.noreply.github.com>
2022-02-16 21:07:10 -0300

    update

--- ex1004.cpp
+++ ex1004.cpp
@@ -1,7 +1,7 @@
 /*Read two integer values. After this, calculate the product between them and store the result in a variable named PROD. 
 Print the result like the example below. Do not forget to print the end of line after the result, otherwise you will receive “Presentation Error”.*/ 
 
-#include <stdio.h>
+#include <stdio.h> 
 
 int main (){
     double a, b, produto, divisao, soma, subtracao;
