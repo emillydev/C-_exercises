5db02438bda6c565d11c58ef692a1c4b2d0454ff
Linek Reis <linekreis@hotmail.com>
2022-02-22 16:23:34 -0300

    Update while.cpp

--- while.cpp
+++ while.cpp
@@ -1,25 +1,26 @@
 #include<stdio.h>
+#include<stdlib.h> // biblioteca necessária para que a função de limpar a tela funcione
 #include<ctype.h>
 // enquanto o usuario digitar algo faça isso
 //posso não executar nenhuma vez
 int main(){
-    int a, b, r;
-    char opcao;
-    scanf("%c", &opcao);
-    fflush(stdin);
-
-    while (opcao == 's' || opcao == 'S')
-    {
+    int a, b , r;
+    int opcao;
+    
+    scanf("%d",&opcao);
+        
+    while (opcao == 1){
         printf("Somar\n");
-        printf("Digite o valor de a: ", a);
-        scanf("%d", &a);
-        printf("Digite o valor de b: ", b);
-        scanf("%d", &b);
-        r = a + b;
-        printf("%d\n",r);
-        printf("Deseja realizar um novo calculo?\n");
-        getchar(opcao);
+        printf("Digite o valor de a: ");
+        scanf("%d",&a);
+        printf("Digite o valor de b: ");
+        scanf("%d",&b);
+        r = a+b;
+        printf("Deseja realizar um novo calculo ?\nDigite 1 para sim ou digite qualquer tecla para finalizar: ");
+        scanf("%d",&opcao);
         fflush(stdin);
+        system("cls"); // função de limpar tela do terminal 
     }
     
+    return 0;
 }
